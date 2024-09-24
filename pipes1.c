3c4450062bc22c662316d4ef1390ee380828abc4
rodriguezrr2004 <rodriguezrr200428@gmail.com>
2024-09-23 21:48:26 -0600

    pipes1.c

--- pipes1.c
+++ pipes1.c
@@ -1,3 +1,5 @@
+//El problema con el codigo es que se tenia un for duplicado como el de la linea 10
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -5,7 +7,7 @@
 int main (int argc, char *argv[]) {
     int i;
     printf("Ejecutando el programa invocador (execprog1). Sus argumentos son:\n");
-    for (i = 0; i < argc; i++) { // el problema era un for duplicado porque el diego esta bien menso
+    for (i = 0; i < argc; i++) { // el problema era un for duplicado que ya se elimino
         printf("argv[%d]: %s\n", i, argv[i]);
         sleep(10);
         strcpy(argv[0], "execprog2");
